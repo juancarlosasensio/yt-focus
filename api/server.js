1cf2de3e25748dcde99e3dce48e11f7ad918bb2a
Juan Carlos Asensio <jasensio@ufm.edu>
2022-12-28 07:28:46 -0500

    Add security middleware

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
-// const cors = require('cors')
-const app = require('express')()
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+// adding Helmet to enhance API's security
+app.use(helmet());
+// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
+// enabling CORS for all requests
+app.use(cors());
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
 const getArticlesByQuery = require('./handlers/hackerNews')
 
 // https://stackoverflow.com/questions/60084428/failed-to-fetch-data-from-localhost
