4f6461bffec2ebf6fe378604d58a884676eba387
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-09 16:41:59 -0600

    Fix fetch error

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import serialize from "serialize-javascript";
 import ReactDOM from "react-dom/server";
 import * as React from "react";
 import App from "../shared/App";
+import { fetchPopularRepos } from "../shared/api";
 
 const app = express();
 
@@ -10,23 +12,31 @@ app.use(cors());
 app.use(express.static("dist"));
 
 app.get("*", (req, res, next) => {
-  const markup = ReactDOM.renderToString(<App />);
+  fetchPopularRepos()
+    .then((data) => {
+      const markup = ReactDOM.renderToString(
+        <App serverData={data} />
+      )
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>YoutubeFocus with SSR</title>
-        <script src="/bundle.js" defer></script>
-        <link href="/main.css" rel="stylesheet" />
-      </head>
+      res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>SSR with React Router</title>
+            <script src="/bundle.js" defer></script>
+            <link href="/main.css" rel="stylesheet">
+            <script>
+              window.__INITIAL_DATA__ = ${serialize(data)}
+            </script>
+          </head>
 
-      <body>
-        <div id="app">${markup}</div>
-      </body>
-    </html>
-  `);
-});
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+      `)
+    })
+})
 
 const PORT = process.env.PORT || 3000;
 
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,11 @@
 import * as React from "react";
+import Grid from "./Grid";
+import "./styles.css";
 
-export default function App() {
-  return <div>Hello World</div>;
+export default function App({ serverData }) {
+  return (
+    <div>
+      <Grid data={serverData} />
+    </div>
+  );
 }
